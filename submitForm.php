7e92b382ee3eca18f5b68be668a011e4ad921f5a
nushankodikara <nushankodi@gmail.com>
2022-12-13 16:37:45 +0530

    adding data to customer table funciton

--- submitForm.php
+++ submitForm.php
@@ -21,9 +21,30 @@ if ($conn->connect_error) {
 
 echo "Connected successfully";
 
-$sql = "INSERT INTO complaint (Customer_name, Customer_email, Message, Subject) VALUES ('$name','$email','$message','$subject')";
+$sql = "INSERT INTO complaint (Customer_Name, Customer_Email, Message, Subject) VALUES ('$name','$email','$message','$subject')";
 
 if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// check if customer exists from name and email
+$sql = "SELECT * FROM customer WHERE Customer_Name = '$name' AND Customer_Email = '$email'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    // increase customer Repair_Count by 1
+    $sql = "UPDATE customer SET Repair_Count = Repair_Count + 1 WHERE Customer_Name = '$name' AND Customer_Email = '$email'";
+  }
+} else {
+    // Add data to customer table Customer_Name, Customer_Email, Repair_Count
+    $sql = "INSERT INTO customer (Customer_Name, Customer_Email, Repair_Count) VALUES ('$name','$email', 1) ON DUPLICATE KEY UPDATE Repair_Count = Repair_Count + 1";
+}
+
+if ($conn-> query($sql) === TRUE) {
   echo "New record created successfully";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
